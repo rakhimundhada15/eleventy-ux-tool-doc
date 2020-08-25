8af20889ad4318cdafc805cb894aeb27879aa09e
rakhi.mundhada <rakhi.mundhada@alertlogic.com>
2020-08-25 17:52:39 +0530

    Deployed using gh-pages

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,14 @@ const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
   eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.addFilter('getkeys',function(object){
+    if(Object.prototype.toString.call( object ) === '[object Array]')
+    for (var i = object.length - 1; i >= 0; i--) {
+        return Object.keys(object[i])
+    }else{
+        return Object.keys(object)
+    };
+})
   return {
       addPassthroughCopy :true,
       dir :{
